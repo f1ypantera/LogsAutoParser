c74fd067f361ca41d8957c43e7f04fd820279fe6
Vadym Tselikin <vadym.tselikin@KBP1-LHP-F76802>
2020-02-24 11:20:05 +0200

    Commit

--- LogsAutoParser/Classes/ResultWriter.cs
+++ LogsAutoParser/Classes/ResultWriter.cs
@@ -49,6 +49,5 @@ namespace LogsAutoParser.Classes
                     break;
             }
         }
-    
     }
 }
---
+++ ProgramWorkflow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LogsAutoParser.Interfaces;
+using Unity;
+
+namespace LogsAutoParser.Workflow
+{
+    class ProgramWorkflow:IRun
+    {
+        private readonly IResultWriter _workFlowReport;
+        
+        [InjectionConstructor]
+        public ProgramWorkflow(IResultWriter workFlowReport)
+        {
+            this._workFlowReport = workFlowReport;
+        }
+        public void Run()
+        {
+            _workFlowReport.Display();
+        }
+    }
+}
